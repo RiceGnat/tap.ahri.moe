bcee4aa713db44dc56f9c51055ce2af20b2b35cd
RiceGnat <ricegnat@gmail.com>
2022-02-12 02:06:52 -0600

    catching dotenv

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-require("dotenv").config();
+try {
+    require("dotenv").config();
+}
+catch (err) {
+    console.log('dotenv not loaded');
+}
 
 const express = require("express");
 const port = (process.env.PORT || 8080);
